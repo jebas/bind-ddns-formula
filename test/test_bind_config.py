91fd564086b54528752161a2383a034c909655b9
Jeff Baskin <jeff.l.baskin@gmail.com>
2017-03-21 16:03:55 -0400

    Initial commit.

---
+++ test/test_bind_config.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+import os, unittest
+from jinja2 import Environment, FileSystemLoader
+
+class TestBindConfig(unittest.TestCase):
+
+    def setUp(self):
+        self.t_dir = os.path.abspath(os.path.join(
+            os.path.dirname(__file__),
+            os.pardir,
+            'bind-ddns',
+            'templates'))
+        self.t_conf = Environment(loader=FileSystemLoader(self.t_dir), 
+            trim_blocks=True)
+
+    def renderTest(self, data, result):
+        holder = self.t_conf.get_template('test_config.jinja').render(testdata=data)
+        output = repr(holder) + ' did not equal ' + repr(result)
+        self.assertEqual(holder, result, output)
+
+    def test_string(self):
+        testdata = 'stuff'
+        result = 'stuff;\n'
+        self.renderTest(testdata, result)
+
+if __name__ == '__main__':
+    unittest.main()
